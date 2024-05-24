fcbccb5371f21279f1fb00f34df47c0e2a4d950b
lgltan <lgltan20@gmail.com>
2024-05-24 10:23:59 +0800

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
-const PORT = 8800
+const PORT = process.env.PORT
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"test"
+})
 
 app.listen(PORT, () => {
     console.log("Listening on port: ", PORT)
 })
+
+// add app.get here
