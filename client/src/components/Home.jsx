2d80bef06d8cad354f1fa0bdcaffc5198e807958
lgltan <lgltan20@gmail.com>
2024-06-14 09:24:48 +0800

    added session token

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import Lobby from './game/Lobby.jsx';
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "./authProvider";
 
 const Home = () => {
     const [activeTab, setActiveTab] = useState('profile');
     const [searchUser, setSearchUser] = useState('');
     const [searchLog, setSearchLog] = useState('');
+
+    const { token } = useAuth();
+
+    if (!token) {
+        // If not authenticated, redirect to the login page
+        return <Navigate to="/" />;
+    }
 
     const renderContent = () => {
         switch (activeTab) {
