eed812df8ab4bdbfc67b309155c818f07b019eea
lgltan <lgltan20@gmail.com>
2024-07-16 18:22:29 +0800

    fixed game frontend

--- client/src/components/game/ChessGame.jsx
+++ client/src/components/game/ChessGame.jsx
@@ -2,13 +2,47 @@
 // p2 is either the player or the bot
 // receive player color - compare player id with currently logged in to player 1 or player 2 in game state
 // insert game logic which sends the board state representation as a string as soon as the piece is dropped, wait for response from server to see if it is a valid move
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
+
+import wK from './pieces/wK.png';
+import wQ from './pieces/wQ.png';
+import wR from './pieces/wR.png';
+import wB from './pieces/wB.png';
+import wN from './pieces/wN.png';
+import wP from './pieces/wP.png';
+
+import bK from './pieces/bK.png';
+import bQ from './pieces/bQ.png';
+import bR from './pieces/bR.png';
+import bB from './pieces/bB.png';
+import bN from './pieces/bN.png';
+import bP from './pieces/bP.png';
 
 const ChessGame = () => {
     const [fen, setFen] = useState('start');
     const [game, setGame] = useState(new Chess());
+
+    const pieces = ["wP","wN","wB","wR","wQ","wK","bP","bN","bB","bR","bQ","bK"];
+    const pieceImgs = [wP,wN,wB,wR,wQ,wK,bP,bN,bB,bR,bQ,bK]
+
+    const customPieces = useMemo(() => {
+        const pieceComponents = {};
+        pieces.forEach((piece, i) => {
+            pieceComponents[piece] = ({ squareWidth }) => (
+                <div
+                    style={{
+                        width: squareWidth,
+                        height: squareWidth,
+                        backgroundImage: `url(${pieceImgs[i]})`,
+                        backgroundSize: "100%"
+                    }}
+                />
+            );
+        });
+        return pieceComponents;
+    }, []);
 
     const onDrop = ({ sourceSquare, targetSquare }) => {
         // Determine if the move involves a pawn promotion
@@ -38,7 +72,16 @@ const ChessGame = () => {
         }
     };
 
-    return <Chessboard position={fen} onDrop={onDrop} />;
+    return <Chessboard 
+    id="chessboard"
+    position={fen} 
+    onDrop={onDrop} 
+    boardOrientation={"black"} 
+    customPieces={customPieces}
+    customDarkSquareStyle={{backgroundColor: '#405A86'}} 
+    customLightSquareStyle={{backgroundColor: '#F7F7F7'}} 
+    customBoardStyle={{border: '16px solid rgb(200, 200, 200)', margin: '20px'}}
+    />;
 };
 
 export default ChessGame;
--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -5,7 +5,12 @@ import './game.css'
 function Game() {
     return (
         <div className="game">
-            <ChessGame />
+            <div className="game-left-col">
+              Nyanner
+            </div>
+            <div className="chessboard-container">
+              <ChessGame />
+            </div>
         </div>
     );
 }
